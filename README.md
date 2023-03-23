9c195403e72dd6fe2fabda3b25268355af7f2b19
Absolute-Value <ji.kei1220@gmail.com>
2023-03-24 03:01:29 +0900

    update test_yolov5.py

--- README.md
+++ README.md
@@ -7,6 +7,26 @@
 ### test_yolov5.py
 Pathを与えた画像に対し，YOLOで物体検出して出力
 
+入力
+![入力](input/umb.jpg)
+
+```console
+$ python test_yolov5.py 
+YOLOv5 🚀 2023-3-22 Python-3.7.15 torch-1.12.1+cu116 CUDA:0 (NVIDIA GeForce GTX 1080 Ti, 11177MiB)
+
+Fusing layers... 
+YOLOv5l summary: 367 layers, 46533693 parameters, 0 gradients
+Adding AutoShape... 
+umb.jpg
+270.09024 175.18948 390.8829 424.11844 0.9521129 0 person
+163.85829 159.12924 284.7193 422.369 0.94140965 0 person
+134.10526 70.76251 404.49503 244.57758 0.787775 25 umbrella
+Saved 1 image to output
+```
+
+出力
+![出力](output/umb.jpg)
+
 ### camera_yolov5.py
 カメラの映像を用いて，YOLOで物体検出して出力
 
--- test_yolov5.py
+++ test_yolov5.py
@@ -3,17 +3,18 @@ import torch
 import cv2
 
 parser = argparse.ArgumentParser(description='test_yolo')
-parser.add_argument('--img_path', type=str, default='/data/dataset/jikuya/imagenet/val/n03706229/ILSVRC2012_val_00001229.JPEG')
-parser.add_argument('--output_img_path', type=str, default='origins')
-parser.add_argument('--output_path', type=str, default='outputs')
+parser.add_argument('--img_path', type=str, default='input/umb.jpg')
+# parser.add_argument('--output_img_path', type=str, default='input')
+parser.add_argument('--output_path', type=str, default='output')
 args = parser.parse_args()
 
-os.makedirs(args.output_img_path, exist_ok=True)
+# os.makedirs(args.output_img_path, exist_ok=True)
 img_name = args.img_path.split('/')[-1]
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
 img = cv2.imread(args.img_path)
-cv2.imwrite(os.path.join(args.output_img_path, img_name), img)
+# cv2.imwrite(os.path.join(args.output_img_path, img_name), img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 result = model(img)
 result.files[0] = img_name
