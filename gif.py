ef01975cf496415326b860112de2df2cda2415b6
Absolute-Value <ji.kei1220@gmail.com>
2023-03-24 16:22:41 +0900

    gitにできるように

---
+++ gif.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os
+import glob
+ 
+# GIFアニメーションを作成
+def create_gif(in_dir, out_filename):
+    path_list = sorted(glob.glob(os.path.join(*[in_dir, '14*']))) # ファイルパスをソートしてリストする
+    imgs = []                                                   # 画像をappendするための空配列を定義
+ 
+    # ファイルのフルパスからファイル名と拡張子を抽出
+    for i in range(len(path_list)):
+        img = Image.open(path_list[i])                          # 画像ファイルを1つずつ開く
+        imgs.append(img)                                        # 画像をappendで配列に格納していく
+ 
+    # appendした画像配列をGIFにする。durationで持続時間、loopでループ数を指定可能。
+    imgs[0].save(out_filename,
+                 save_all=True, append_images=imgs[1:], optimize=False, duration=100, loop=0)
+ 
+# GIFアニメーションを作成する関数を実行する
+create_gif(in_dir='outputs/2023-03-24', out_filename='2023-03-24_14.gif')
